3398d0c77ee59fde4edc5fd356425c42aaa90464
Anthony <filippea@oregonstate.edu>
2021-03-06 15:41:46 -0800

    Added FizzBuzz Coverage
    Fizzbuzz is now returned by the function for numbers divisible by 5 and 3

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,4 +1,6 @@
 def SingleNumber(x):
+    if(((x/3.0) == round(x/3.0)) and ((x/5.0) == round(x/5.0))):
+        return "FizzBuzz"
     if((x/3.0) == round(x/3.0)):
         return "Fizz"
     if((x/5.0) == round(x/5.0)):
